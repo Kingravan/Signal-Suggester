c38ad69d2ec467b921939ba5e2f8e9beb3c1f3bf
Kingravan <49265172+Kingravan@users.noreply.github.com>
2022-04-26 18:52:55 +0530

    Update Currencies.java

--- Signal_Suggester/src/signal_suggester/Currencies.java
+++ Signal_Suggester/src/signal_suggester/Currencies.java
@@ -55,7 +55,7 @@ public class Currencies implements ActionListener{
         String[] timeFrame_values = {"1month", "3month", "6month", "1year", "2year", "3year", "5year"};
         time_Frame = new JComboBox(timeFrame_values);
         interval = new JLabel("Interval: ");
-        String[] interval_values = {"1min", "5min", "15min", "30min", "1hr", "1.5hr", "1day", "5day", "1week", "1month", "3month"};
+        String[] interval_values = {"1day", "5day", "1week", "1month", "3month"};
         Interval = new JComboBox(interval_values);
         maPeriod = new JLabel("MA Period: ");
         fmavalues = new SpinnerNumberModel(1, 1, null, 1);
@@ -93,7 +93,7 @@ public class Currencies implements ActionListener{
         firstMA.setValue(20);
         secondMA.setValue(50);
         time_Frame.setSelectedIndex(4);
-        Interval.setSelectedIndex(6);
+        Interval.setSelectedIndex(0);
         
         inputTicker = new JTextField();
         inputTicker.setFont(new Font("Ariel", Font.PLAIN, 16));
@@ -334,17 +334,11 @@ public class Currencies implements ActionListener{
             String finalInterval = null;
             switch(tempInterval)
             {
-                case 0 -> finalInterval = "1m";
-                case 1 -> finalInterval = "5m";
-                case 2 -> finalInterval = "15m";
-                case 3 -> finalInterval = "30m";
-                case 4 -> finalInterval = "1h";
-                case 5 -> finalInterval = "90m";
-                case 6 -> finalInterval = "1d";
-                case 7 -> finalInterval = "5d";
-                case 8 -> finalInterval = "1wk";
-                case 9 -> finalInterval = "1mo";
-                case 10 -> finalInterval = "3mo";
+                case 0 -> finalInterval = "1d";
+                case 1 -> finalInterval = "5d";
+                case 2 -> finalInterval = "1wk";
+                case 3 -> finalInterval = "1mo";
+                case 4 -> finalInterval = "3mo";
             }
                 String[] List;
                 if(Ticker.contains(","))
